af1b7a00b9cfbe54e17976ad68e534162391b261
gustavo867 <tartatavo3@gmail.com>
2020-12-17 02:05:18 -0300

    :label: updating types

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -3,11 +3,15 @@ export const darkTheme = {
   primary: '#21262E',
   text_color: '#FFF',
   status_bar_color: 'light-content',
+  switch_mode: '#13131A',
+  product_color: '#FFF',
 };
 
 export const lightTheme = {
   mode: 'light',
   primary: '#6F737B',
-  text_color: '#21262E',
+  text_color: '#000',
   status_bar_color: 'dark-content',
+  switch_mode: '#FFF',
+  product_color: '#FFF',
 };
--- src/themes/theme.types.ts
+++ src/themes/theme.types.ts
@@ -3,4 +3,10 @@ export interface Theme {
   primary: string;
   text_color: string;
   status_bar_color: 'light-content' | 'dark-content';
+  switch_mode: string;
+  product_color: string;
+}
+
+export interface ThemeProps {
+  theme: Theme;
 }
