3486e2ac9b8995519e786ddef6eafcbac7f1ca37
gustavo867 <tartatavo3@gmail.com>
2020-12-16 22:58:16 -0300

    :hammer: theme

---
+++ src/themes/index.ts
@@ -0,0 +1,13 @@
+export const darkTheme = {
+  mode: 'dark',
+  primary: '#21262E',
+  text_color: '#FFF',
+  status_bar_color: 'light-content',
+};
+
+export const lightTheme = {
+  mode: 'light',
+  primary: '#6F737B',
+  text_color: '#21262E',
+  status_bar_color: 'dark-content',
+};
---
+++ src/themes/theme.types.ts
@@ -0,0 +1,6 @@
+export interface Theme {
+  mode: string;
+  primary: string;
+  text_color: string;
+  status_bar_color: 'light-content' | 'dark-content';
+}
