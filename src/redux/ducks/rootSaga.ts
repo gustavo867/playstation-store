4868023525c89fc71fb336c8ff389b055a893e86
gustavo867 <tartatavo3@gmail.com>
2020-12-16 23:41:34 -0300

    :tada: redux

---
+++ src/redux/ducks/rootSaga.ts
@@ -0,0 +1,7 @@
+import { all, takeLatest } from 'redux-saga/effects';
+import { ProductsTypes } from './products/types';
+import { load } from './products/sagas';
+
+export default function* rootSaga() {
+  return yield all([takeLatest(ProductsTypes.LOAD_REQUEST, load)]);
+}
